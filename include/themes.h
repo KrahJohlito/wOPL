9e3f7c741bbf234d7a582f49adbea3e438ef4d10
KrahJohlito <KrahJohlito@outlook.com>
2026-05-23 16:58:10 +0930

    might look a bit shit, test to see if scale needs to change based on t

--- include/themes.h
+++ include/themes.h
@@ -159,7 +159,6 @@ int thmFindGuiID(const char *theme);
 const char **thmGetGuiList(void);
 char *thmGetFilePath(int themeID);
 
-extern int isAnimating;
-extern int animationDirection;
+extern void thmTriggerCoverflowAnim(int direction);
 
 #endif
