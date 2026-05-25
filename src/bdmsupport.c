e8d055e671adedad70b4f0f1d2b918b7c968efd1
KrahJohlito <KrahJohlito@outlook.com>
2026-05-25 10:40:09 +0930

    tisk

--- src/bdmsupport.c
+++ src/bdmsupport.c
@@ -921,19 +921,20 @@ int bdmUpdateDeviceData(item_list_t *itemList)
         fileXioDclose(dir);
         return 1;
     } else if (dir < 0 && visible == 1) {
-		    pDeviceData->disconnectCounter++;
+            pDeviceData->disconnectCounter++;
 
-        if (pDeviceData->disconnectCounter < 20) {
-            // Device has been removed, make the menu item invisible. We can't really cleanup resources (like the game list) just yet
-            // as we don't know if the data is being used asynchronously.
-            if (itemList->owner != NULL) {
-                LOG("bdmUpdateDeviceData: setting device %d invisible\n", itemList->mode);
-                ((opl_io_module_t *)itemList->owner)->menuItem.visible = 0;
-            }
+        if (pDeviceData->disconnectCounter < 20)
+            return 0;
 
-            LOG("Mass device: %d (%d) disconnected\n", itemList->mode, pDeviceData->massDeviceIndex);
-            return -1;
+        // Device has been removed, make the menu item invisible. We can't really cleanup resources (like the game list) just yet
+        // as we don't know if the data is being used asynchronously.
+        if (itemList->owner != NULL) {
+            LOG("bdmUpdateDeviceData: setting device %d invisible\n", itemList->mode);
+            ((opl_io_module_t *)itemList->owner)->menuItem.visible = 0;
         }
+
+        LOG("Mass device: %d (%d) disconnected\n", itemList->mode, pDeviceData->massDeviceIndex);
+        return -1;
     }
 
     // No change to the device state detected.
