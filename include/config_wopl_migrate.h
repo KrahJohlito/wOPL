f1ad7aeb72a6e09216fafff44a4636327f4cad19
KrahJohlito <KrahJohlito@outlook.com>
2026-05-28 12:33:23 +0930

    need to split them to avoid conflict for now

---
+++ include/config_wopl_migrate.h
@@ -0,0 +1,10 @@
+/*
+  TODO: delete this once migration complete)
+*/
+
+#ifndef CONFIG_WOPL_MIGRATE_H
+#define CONFIG_WOPL_MIGRATE_H
+
+int config_wopl_migrate(const char *path, int *out_theme_id, int *out_lang_id);
+
+#endif
