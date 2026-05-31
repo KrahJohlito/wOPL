78d54f0b57eb43b2699ceaa80054744242a4c9c3
KrahJohlito <KrahJohlito@outlook.com>
2026-06-01 09:02:24 +0930

    coverflow: add options to select either 3 or 5 display covers

--- include/themes.h
+++ include/themes.h
@@ -148,6 +148,11 @@ extern theme_t *gTheme;
 
 extern int gDiscEnableArt;
 
+extern int gCoverflowCount;
+extern int gCoverflowCenterScale;
+extern int gCoverflowAnimSpeed;
+extern int gCoverflowDimCovers;
+
 void thmInit(void);
 void thmReinit(const char *path);
 void thmReloadScreenExtents(void);
