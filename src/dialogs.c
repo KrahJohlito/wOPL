0caabff5f85374be35ef30e5c11cc21c5a3f2602
KrahJohlito <KrahJohlito@outlook.com>
2026-06-01 09:59:31 +0930

    spacer innit

--- src/dialogs.c
+++ src/dialogs.c
@@ -343,8 +343,8 @@ struct UIItem diaUIConfig[] = {
     {UI_SPACER},
     {UI_ENUM, UICFG_THEME, 1, 1, -1, 0, 0, {.intvalue = {0, 0}}},
     {UI_BREAK},
-
-    {UI_BUTTON, UICFG_COVERFLOW_BUTTON, 1, 1, -1, -40, 0, {.label = {"Coverflow Settings", -1}}},
+    {UI_SPACER},
+    {UI_BUTTON, UICFG_COVERFLOW_BUTTON, 1, 1, -1, 0, 0, {.label = {"Coverflow Settings", -1}}},
     {UI_BREAK},
 
     {UI_LABEL, 0, 1, 1, -1, -40, 0, {.label = {NULL, _STR_LANGUAGE}}},
