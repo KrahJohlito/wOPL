784246f965211ce5b652debbd6dcd66fdf3fd6c0
KrahJohlito <KrahJohlito@outlook.com>
2026-06-01 15:06:01 +0930

    fix spacing in ws for 5 cov

--- src/themes.c
+++ src/themes.c
@@ -1093,7 +1093,12 @@ static void drawCoverFlow(struct menu_list *menu, struct submenu_list *item, con
         coverSpacing = rmWideScale(coverSpacing);
 
     int coverDistance = coverWidth + coverSpacing;
-    int basePosX = (coverSpacing << 1) + (coverWidth >> 1);
+    int basePosX;
+    if (gWideScreen) {
+        int totalGroupWidth = (coverCount - 1) * coverDistance + coverWidth;
+        basePosX = (screenWidth - totalGroupWidth) / 2 + (coverWidth >> 1);
+    } else
+        basePosX = (coverSpacing << 1) + (coverWidth >> 1);
 
     struct
     {
