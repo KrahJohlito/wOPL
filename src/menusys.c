00da2e86768595a21fa2efc7c8cf14e48082faf6
KrahJohlito <KrahJohlito@outlook.com>
2026-06-05 15:19:42 +0930

    save global

--- src/menusys.c
+++ src/menusys.c
@@ -1284,6 +1284,7 @@ void menuHandleInputGameMenu()
             guiGameSaveConfig(&itemPgCfg, selected_item->item->userdata);
             itemPgCfg.config_source = CONFIG_SOURCE_USER;
             menuSaveConfig();
+            wOPLGlobalGameSave();
             guiMsgBox(_l(_STR_GAME_SETTINGS_SAVED), 0, NULL);
             guiGameLoadConfig(selected_item->item->userdata, gameMenuLoadConfig(NULL));
         } else if (menuID == GAME_TEST_CHANGES) {
