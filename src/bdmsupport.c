41831ee6ca50c7fa4854917c75c8f87825c19a41
KrahJohlito <KrahJohlito@outlook.com>
2026-06-09 11:51:50 +0930

    a

--- src/bdmsupport.c
+++ src/bdmsupport.c
@@ -189,7 +189,7 @@ static void bdmInit(item_list_t *itemList)
     pDeviceData->bdmGameCount = 0;
     pDeviceData->bdmGames = NULL;
     bdmLoadModules();
-    itemList->delay = gBDMFramesDelay;
+    //itemList->delay = gBDMFramesDelay;
     itemList->enabled = 1;
 }
 
