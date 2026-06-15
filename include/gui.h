63d0c9294052d8147dedfdeb9965eeaa803f818d
KrahJohlito <KrahJohlito@outlook.com>
2026-06-16 09:27:59 +0930

    add splash boot notifications and version text

--- include/gui.h
+++ include/gui.h
@@ -155,6 +155,10 @@ void guiCheckNotifications(int checkTheme, int checkLang);
 
 void guiShowCfgMigration(void); // DELETE_WITH_MIGRATION
 
+void guiSetBootStatus(const char *status);
+void guiSetBootStatusIfActive(const char *status);
+void guiShowBootStatus(const char *status);
+
 /** Renders the given string on screen for the given function until it's io finishes
  * @note The ptr pointer is watched for it's value. The IO is considered finished when the value becomes zero.
  * @param ptr The finished state pointer (1 unfinished, 0 finished)
