71e21246c235f228146b1ec48939aa21c4379a63
KrahJohlito <KrahJohlito@outlook.com>
2026-06-22 10:32:26 +0930

    path: add boot path support

---
+++ include/pathsupport.h
@@ -0,0 +1,14 @@
+#ifndef PATHSUPPORT_H
+#define PATHSUPPORT_H
+
+#include <stddef.h>
+
+void pathSetLaunchPath(const char *path);
+const char *pathGetLaunchPath(void);
+
+int pathIsDevicePath(const char *path);
+void pathNormaliseDir(char *dir, size_t dir_len);
+int pathGetBootDir(char *dir_out, size_t dir_len);
+int pathJoin(char *out, size_t out_len, const char *dir, const char *name);
+
+#endif
