87de59c31b5d58bebe39b0c992536c41a52b7d18
KrahJohlito <KrahJohlito@outlook.com>
2026-06-26 21:28:04 +0930

    config: save bootstrap config

--- src/config_wopl.c
+++ src/config_wopl.c
@@ -59,6 +59,7 @@
 #define BOOT_FILENAME "wopl_boot.cfg"
 
 static char config_dir[128] = {0};
+static char boot_dir[128] = {0};
 static char last_played[256] = {0};
 
 static char s_theme_name[128] = {0};
@@ -541,7 +542,7 @@ static int normalise_true_config_dir(char *out, size_t out_len, const char *dir,
     return 1;
 }
 
-static int load_boot_config_from_dir(const char *boot_dir)
+static int load_boot_config_from_dir(const char *launch_dir)
 {
     char boot_root[128];
     char boot_path[256];
@@ -550,12 +551,12 @@ static int load_boot_config_from_dir(const char *boot_dir)
     config_t cfg;
     int have_config_dir = 0;
 
-    if (!boot_dir || !boot_dir[0])
+    if (!launch_dir || !launch_dir[0])
         return 0;
 
     // Prepare the boot/cwd root before trying to read wopl_boot.cfg from it
     // Legacy massN: is allowed here only because this path came from argv0/cwd
-    if (!normalise_true_config_dir(boot_root, sizeof(boot_root), boot_dir, 1))
+    if (!normalise_true_config_dir(boot_root, sizeof(boot_root), launch_dir, 1))
         return 0;
 
     copy_str(boot_dir, boot_root, sizeof(boot_dir));
@@ -615,6 +616,7 @@ static int pick_default_config_dir(void)
             return 1;
 
         if (normalise_true_config_dir(true_dir, sizeof(true_dir), dir, 1)) {
+            copy_str(boot_dir, true_dir, sizeof(boot_dir));
             copy_str(config_dir, true_dir, sizeof(config_dir));
             configEarlyLog("CONFIG: using boot/cwd config_dir='%s'\n", config_dir);
             return 1;
@@ -1862,9 +1864,24 @@ int configLoad(int types)
     return lscret;
 }
 
+static int config_type_count(int types)
+{
+    int count = 0;
+
+    if (types & CONFIG_OPL)
+        count++;
+    if (types & CONFIG_NETWORK)
+        count++;
+    if (types & CONFIG_GAME)
+        count++;
+
+    return count;
+}
+
 static int save_all_to_current_dir(int types) // like the old configWriteMulti()
 {
     int result = 0;
+    int expected = config_type_count(types);
 
     if (!ensure_config_dir()) {
         LOG("CONFIG: no config_dir selected for save\n");
@@ -1888,6 +1905,12 @@ static int save_all_to_current_dir(int types) // like the old configWriteMulti()
         result += do_save_at_dir(config_dir, NET_FILENAME, build_net);
     if (types & CONFIG_GAME)
         result += do_save_at_dir(config_dir, GAME_FILENAME, build_global_game);
+
+    if (result != expected)
+        return result;
+
+    if (!save_boot_config())
+        return 0;
 
     return result;
 }
