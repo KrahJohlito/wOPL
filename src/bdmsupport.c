a9e739d629a36790c59c34644ab8f0569e21e216
KrahJohlito <KrahJohlito@outlook.com>
2026-06-29 12:00:08 +0930

    ilink test

--- src/bdmsupport.c
+++ src/bdmsupport.c
@@ -268,7 +268,7 @@ void bdmLoadModulesForLegacyMass(void)
 
     WaitSema(bdmLoadModuleLock);
     bdmLoadUSBModules();
-    bdmLoadiLinkModules();
+    //bdmLoadiLinkModules();
     bdmLoadMX4SIOModules();
     bdmLoadBdmHDDModules();
     SignalSema(bdmLoadModuleLock);
